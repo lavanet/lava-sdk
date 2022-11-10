6b07ae60f64b7e6e4143e7f6e5396443b6deb720
Aleksao998 <aleksaopacic988@gmail.com>
2022-11-10 11:32:20 +0100

    Add sdk example

--- README.md
+++ README.md
@@ -74,12 +74,14 @@ Download the latest Lava binary from https://github.com/lavanet/lava/releases
     ```
     
 ## How to use it 
-Current lava-sdk implementation can be used only with the terminal. 
+Current lava-sdk implementation can be used with the terminal and we have an example folder to demonstrate how the sdk will be used 
 
     ```md
     !!! Caution
     Before continuing to follow the guide, please make sure you finished all the steps in the Prerequisites section
     ```
+
+### Terminal
 
 1. List all supported commands
 
@@ -111,3 +113,20 @@ Current lava-sdk implementation can be used only with the terminal.
     ```
 
     ![gif-maker-imgflip-10-november-2022_58WOWHr5_AdobeExpress (1)](https://user-images.githubusercontent.com/42786413/201049338-918c1112-ab74-4258-bdfe-61cea82b80dc.gif)
+
+### Example
+
+All examples are located in the **./src/examples** folder. Currently we support only running **./src/examples/basic.ts** out of the box. 
+1. Compile ts -> js
+
+    ```bash
+    yarn build
+    ```
+
+2. Run example/basic.ts 
+
+    ```bash
+    yarn example
+    ```
+
+basic example can be executed with the **yarn example** command
---
+++ src/examples/basic.ts
@@ -0,0 +1,51 @@
+/* 
+                    Basic example
+        This file shows basic usage of the future polygon-sdk library
+        Currently we have implemented:
+        1. Recreating account from mnemonic
+        2. Fetching paring list (without epoch and maxcu attribute)
+*/
+import Logger from '../logger/logger'
+
+// Fetch from package
+import LavaWallet from "../wallet/wallet"
+import LavaConsumer from "../consumer/consumer"
+
+async function run() {
+    const mnemonic = "client marine special phone fury cry little bar loop soap kiwi kick donate pattern curious spatial grab attend board tuna add famous head crystal"
+    const endpoint = "http://44.205.140.46:26657"
+    const chainID = "ETH1"
+    const rpcInterface = "jsonrpc"
+
+    // Create wallet
+    const wallet = new LavaWallet(mnemonic);
+
+    // Initialize wallet
+    await wallet.init();
+
+    // get account from wallet
+    const account = await wallet.getConsumerAccount();
+
+    // print account detail
+    wallet.printAccount(account);
+
+    // Create consumer 
+    const consumer = new LavaConsumer(endpoint, chainID, rpcInterface);
+
+    // Initialize consumer 
+    await consumer.init(account);
+
+    // Get paring list
+    const paring = await consumer.getPairing();
+
+    // Print paring
+    consumer.printPairingList(paring);
+}
+
+
+
+run()
+    .then()
+    .catch((err) => {
+        Logger.error(err);
+    });
