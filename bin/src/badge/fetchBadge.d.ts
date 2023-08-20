ab5a09e5b7a5ea03f8568e018707194c78ba8a0d
Ran Mishael <ran@lavanet.xyz>
2023-08-20 15:09:56 +0200

    redirecting repository to lava mono repo

--- bin/src/badge/fetchBadge.d.ts
+++
@@ -1,2 +0,0 @@
-import { GenerateBadgeResponse } from "../grpc_web_services/pairing/badges_pb";
-export declare function fetchBadge(serverAddress: string, badgeUser: string, projectKey: string): Promise<GenerateBadgeResponse>;
